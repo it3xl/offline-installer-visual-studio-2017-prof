0f7d9cc6213db835243b45cbe0182dfa2466b942
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-03-30 17:40:18 +0300

    Initial commit.

---
+++ create-update offline packages.bat
@@ -0,0 +1,10 @@
+@REM  it3xl.com
+
+
+@REM Source for the bootstrapper of VS
+@REM https://www.visualstudio.com/vs/
+START .\web_installer\vs_professional.exe  --layout .\packages  --lang en-US
+
+
+
+
---
+++ install VS 2017 Prof.bat
@@ -0,0 +1,10 @@
+@REM  it3xl.com
+
+@REM 
+@ECHO OFF
+
+ECHO.
+@CALL .\manager\worker.bat
+ECHO. & ECHO ERRORLEVEL is %ERRORLEVEL% & ECHO.
+
+PAUSE
