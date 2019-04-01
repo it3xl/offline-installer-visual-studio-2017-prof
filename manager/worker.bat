98bd15fafe19525fe08c8aef41cbf42701fc612a
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2019-04-01 17:27:28 +0300

    Last Microsoft modifications.

--- manager/worker.bat
+++ manager/worker.bat
@@ -14,20 +14,23 @@ IF NOT EXIST "%invokePath%\packages\vs_setup.exe" (
 SET certmgr="%invokePath%\certmgr\certmgr.exe"
 
 SET cert_path=%invokePath%\packages\certificates
+
 @REM From "Option 3 - Install certificates as part of a scripted deployment of Visual Studio"
 @REM https://docs.microsoft.com/en-us/visualstudio/install/install-certificates-for-visual-studio-offline
-%certmgr% -add -c "%cert_path%\manifestSignCertificates.p12" -n "Microsoft Code Signing PCA 2011" -s -r LocalMachine CA
-IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "Microsoft Code Signing PCA 2011" & EXIT /B 103 )
-%certmgr% -add -c "%cert_path%\manifestSignCertificates.p12" -n "Microsoft Root Certificate Authority" -s -r LocalMachine root
-IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "Microsoft Root Certificate Authority" & EXIT /B 103 )
-%certmgr% -add -c "%cert_path%\manifestCounterSignCertificates.p12" -n "Microsoft Time-Stamp PCA 2010" -s -r LocalMachine CA
-IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "Microsoft Time-Stamp PCA 2010" & EXIT /B 103 )
-%certmgr% -add -c "%cert_path%\manifestCounterSignCertificates.p12" -n "Microsoft Root Certificate Authority" -s -r LocalMachine root
-IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "Microsoft Root Certificate Authority" & EXIT /B 103 )
+
+%certmgr% -add "%cert_path%\manifestRootCertificate.cer" -n "Microsoft Root Certificate Authority 2011" -s -r LocalMachine root
+IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "manifestRootCertificate.cer" & EXIT /B 103 )
+
+%certmgr% -add "%cert_path%\manifestCounterSignRootCertificate.cer" -n "Microsoft Root Certificate Authority 2010" -s -r LocalMachine root
+IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "manifestCounterSignRootCertificate.cer" & EXIT /B 103 )
+
 %certmgr% -add -c "%cert_path%\vs_installer_opc.SignCertificates.p12" -n "Microsoft Code Signing PCA" -s -r LocalMachine CA
-IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "Microsoft Code Signing PCA" & EXIT /B 103 )
+IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "1 vs_installer_opc.SignCertificates.p12" & EXIT /B 103 )
+
 %certmgr% -add -c "%cert_path%\vs_installer_opc.SignCertificates.p12" -n "Microsoft Root Certificate Authority" -s -r LocalMachine root
-IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "Microsoft Root Certificate Authority" & EXIT /B 103 )
+IF %ERRORLEVEL% NEQ 0 ( ECHO EXIT - Fix certificate error for "2 vs_installer_opc.SignCertificates.p12" & EXIT /B 103 )
+
+
 
 
 ECHO. & ECHO Start Visula Studio installer. & ECHO.
